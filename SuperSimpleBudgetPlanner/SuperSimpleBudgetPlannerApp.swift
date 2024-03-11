2864eff89f4ff5920d22ad6359ee2f9cc74dbe03
noahbeito <noah.beito@cvent.com>
2024-03-10 17:10:55 -0700

    Initial Commit

---
+++ SuperSimpleBudgetPlanner/SuperSimpleBudgetPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSimpleBudgetPlannerApp.swift
+//  SuperSimpleBudgetPlanner
+//
+//  Created by Noah Beito on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SuperSimpleBudgetPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
